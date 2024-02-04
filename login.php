b7d35fa2023ea503efc7bf2fb7a3374fabace19f
Alysson Santos <salysson280@gmail.com>
2024-02-04 16:42:26 -0300

    update

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if (isset($_POST['email']) && strlen($_POST['email']) > 0){
         $_SESSION['email'] = $mysqli->escape_string($_POST['email']);
         $_SESSION['senha'] = md5(md5($_POST['senha']));
 
-        $sql_code = "SELECT senha, codigo FROM usuario WHERE email = '$_SESSION[email]";
+        $sql_code = "SELECT senha, codigo FROM usuario WHERE email =  $_SESSION[email]";
         $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
         $dado = $sql_query->fetch_assoc();
         $total = $sql_query->num_rows;
@@ -37,6 +37,8 @@ if (isset($_POST['email']) && strlen($_POST['email']) > 0){
     }
 
 
+}
+$email = isset($_SESSION['email']) ? $_SESSION['email'] : null;
 
 
 ?>
@@ -58,7 +60,7 @@ if (isset($_POST['email']) && strlen($_POST['email']) > 0){
             //     }
             ?>
             <form method="POST" action="">
-                <p><input value= <? echo $_SESSION['email']?>  name="E-mail" placeholder="E-mail" type="text" ></p>
+                <p><input value <?= $email?>  name="E-mail" placeholder="E-mail" type="text" ></p>
                 <p><input name="senha"  type="password"></p>
                 <p><a href="">Esqueceu a Senha?</a></p>
                 <p><input value="Entrar" type="submit"></p>
