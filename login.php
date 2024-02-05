81ef88fe768488016d025b96839246411d4e7326
Alysson Santos <salysson280@gmail.com>
2024-02-05 17:29:41 -0300

    update

--- login.php
+++ login.php
@@ -11,6 +11,7 @@ if (isset($_POST['email']) && strlen($_POST['email']) > 0){
         $_SESSION['email'] = $mysqli->escape_string($_POST['email']);
         $_SESSION['senha'] = md5(md5($_POST['senha']));
 
+
         $sql_code = "SELECT senha, codigo FROM usuario WHERE email =  $_SESSION[email]";
         $sql_query = $mysqli->query($sql_code) or die($mysqli->error);
         $dado = $sql_query->fetch_assoc();
@@ -35,10 +36,18 @@ if (isset($_POST['email']) && strlen($_POST['email']) > 0){
     
       
         if (!isset($erro)  || count($erro) == 0){
-            return "<script>alert('Login efetuado com sucesso'); location.href='sucesso.php';</script>";
+            echo "<script>alert(Login efetuado com sucesso); location.href='sucesso.php';</script>";
         }
 
-}
+        header("Location: sucesso.php");
+
+
+
+       
+
+
+    
+    }
 $email = isset($_SESSION['email']) ? $_SESSION['email'] : null;
 
 
